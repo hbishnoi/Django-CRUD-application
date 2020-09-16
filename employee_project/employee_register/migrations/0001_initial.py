246c5446c98075ca710c20f1e20766ff5d0b0b55
hbishnoi <hbishnoi@stevens.edu>
2020-09-16 00:45:51 -0400

    form field changes

--- employee_project/employee_register/migrations/0001_initial.py
+++ employee_project/employee_register/migrations/0001_initial.py
@@ -12,13 +12,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        # migrations.CreateModel(
-        #     name='Position',
-        #     fields=[
-        #         ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-        #         ('title', models.CharField(max_length=100)),
-        #     ],
-        # ),
         migrations.CreateModel(
             name='Employee',
             fields=[
@@ -32,7 +25,6 @@ class Migration(migrations.Migration):
                 ('city', models.CharField(max_length=100)),
                 ('zipCode', models.CharField(max_length=100)),
                 ('state', models.CharField(max_length=100)),
-                # ('position', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='employee_register.position')),
             ],
         ),
     ]
--- employee_project/employee_register/models.py
+++ employee_project/employee_register/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+STATES = (
+    ('', 'Choose...'),
+    ('NY', 'New York'),
+    ('CA', 'California'),
+    ('TX', 'Texas')
+)
+
 class Employee(models.Model):
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField()
     phoneNumber = models.BigIntegerField()
     address = models.CharField(max_length=100)
     address2 = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     zipCode = models.BigIntegerField()
-    state = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, choices=STATES, default='1')
